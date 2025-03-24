1fd3b2bca0f4d65e489dcfd880afd4d4f50f8d81
ritikkumar352 <krritik2002@gmail.com>
2025-03-24 12:47:20 +0530

    first commit

---
+++ src/main/java/com/deploy/httpsdeploytest/HttpsDeployTestApplication.java
@@ -0,0 +1,13 @@
+package com.deploy.httpsdeploytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpsDeployTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HttpsDeployTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/deploy/httpsdeploytest/HttpsDeployTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deploy.httpsdeploytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpsDeployTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
